d532e52c0c9b1ba1268fc0e19a2c717b51ee6e8f
hyunbae.jeon <mazingazz@gmail.com>
2023-07-30 09:15:43 +0900

    init

---
+++ nuxt.config.ts
@@ -0,0 +1,21 @@
+import { defineNuxtConfig } from 'nuxt/config';
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  css: ['bootstrap/dist/css/bootstrap.css', 'bootstrap-vue/dist/bootstrap-vue.css'],
+  ssr: true,
+  devtools: { enabled: true },
+  debug: true,
+  srcDir: 'src/',
+  modules: [['@bootstrap-vue-next/nuxt', {}]],
+  typescript: {
+    shim: false,
+    typeCheck: true,
+  },
+  // https://nitro.unjs.io/guide/community/contributing
+  nitro: {
+    rootDir: 'src/',
+    devServer: {
+      watch: [],
+    },
+  },
+});
---
+++ src/pages/index.vue
@@ -0,0 +1,5 @@
+<script lang="ts" setup></script>
+<template>
+  start
+  <b-form-input id="input-1" type="email" placeholder="Enter email" required></b-form-input>
+</template>
