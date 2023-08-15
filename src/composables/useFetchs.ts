462f2bf22d69c9e4e7c62ac01d487735679def73
hyunbae.jeon <mazingazz@gmail.com>
2023-08-16 08:31:12 +0900

    reddit api

--- src/composables/useFetchs.ts
+++ src/composables/useFetchs.ts
@@ -13,3 +13,18 @@ export const useCafeFetch = async (request, options?) => {
     async onResponseError({ request, response, options }: any) {},
   });
 };
+
+// https://www.reddit.com/r/Wallstreetbets/top.json?limit=5&t=day
+export const useRedditFetch = async (request, options?) => {
+  return await ofetch(request, {
+    baseURL: 'https://www.reddit.com/r/Wallstreetbets/top.json?',
+    withCredentials: true,
+    ...options,
+    async onRequest({ request, options }: any) {},
+    async onRequestError({ request, options, error }) {},
+    async onResponse({ request, response, options }: any) {
+      // console.log('response success >>>> ', response._data);
+    },
+    async onResponseError({ request, response, options }: any) {},
+  });
+};
